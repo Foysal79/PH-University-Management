b778376d4a8018473c86c338e8b516cf228ca6cb
Foysal79 <onthor312001@gmail.com>
2024-09-06 11:18:32 +0600

    first commit

---
+++ src/components/layout/AdminLayout.tsx
@@ -0,0 +1,11 @@
+
+const AdminLayout = () => {
+    return (
+        <div>
+            <h1>This is navar Session </h1>
+            <h3>Admin Lay out</h3>
+        </div>
+    );
+};
+
+export default AdminLayout;
